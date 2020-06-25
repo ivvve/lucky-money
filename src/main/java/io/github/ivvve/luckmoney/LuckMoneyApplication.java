f93a0546a101d1c0a0bed117fbc0d04a9c16c13a
devson <sonyc5720@gmail.com>
2020-06-25 14:37:25 +0900

    init project

---
+++ src/main/java/io/github/ivvve/luckmoney/LuckMoneyApplication.java
@@ -0,0 +1,13 @@
+package io.github.ivvve.luckmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckMoneyApplication.class, args);
+    }
+
+}
